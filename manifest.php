4aa874404bff06e6cf2550e67aaae2ef12552ce2
James Peru Mmbono <jmsperu@gmail.com>
2017-08-31 10:05:44 +0300

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -20,44 +20,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //This file describes the module, including database tables
 
 //Basic variables
-$name="Logs" ; //The name of the variable as it appears to users. Needs to be unique to installation. Also the name of the folder that holds the unit.
-$description="Module for Accessign Gibbon Access Logs" ; //Short text description
-$entryURL="index.php" ; //The landing page for the unit, used in the main menu
-$type="Additional" ; //Do not change.
-$category="" ; //The main menu area to place the module in
-$version="1.0.0.0" ; //Verson number
-$author="James Peru" ; //Your name
-$url="https://xcobean.org" ; //Your URL
-
-//Module tables
-$moduleTables[0]="" ; //One array entry for every database table you need to create. Convention dictates that you preface the table name with the module name, to keep the db neat. 
-$moduleTables[1]="" ; //Other sql can be run, but resulting data will not be cleaned up on uninstall.
-
-//gibbonSettings entries
-$gibbonSetting[0]=""; //One array entry for every gibbonSetting entry you need to create. The scope field for the setting should be your module name.
-$gibbonSetting[1]="";
+$name='Logs' ; //The name of the variable as it appears to users. Needs to be unique to installation. Also the name of the folder that holds the unit.
+$description='Module for Accessing Gibbon Logs' ; //Short text description
+$entryURL='logs_access.php' ; //The landing page for the unit, used in the main menu
+$type='Additional' ; //Do not change.
+$category='' ; //The main menu area to place the module in
+$version='1.0.0.0' ; //Verson number
+$author='Xcobean' ; //Your name
+$url='https://xcobean.org' ; //Your URL
 
 
 //Action rows 
+
+
 //One array per action
-$actionRows[0]["name"]="" ; //The name of the action (appears to user in the right hand side module menu)
-$actionRows[0]["precedence"]="0"; //If it is a grouped action, the precedence controls which is highest action in group
-$actionRows[0]["category"]="" ; //Optional: subgroups for the right hand side module menu
-$actionRows[0]["description"]="" ; //Text description
-$actionRows[0]["URLList"]="" ; //List of pages included in this action
-$actionRows[0]["entryURL"]= 'logs_access.php'; //The landing action for the page.
-$actionRows[0]["entrySidebar"]="Y" ;
-$actionRows[0]["menuShow"]="Y" ;
-$actionRows[0]["defaultPermissionAdmin"]="Y" ; //Default permission for built in role Admin
-$actionRows[0]["defaultPermissionTeacher"]="N" ; //Default permission for built in role Teacher
-$actionRows[0]["defaultPermissionStudent"]="N" ; //Default permission for built in role Student
-$actionRows[0]["defaultPermissionParent"]="N" ; //Default permission for built in role Parent
-$actionRows[0]["defaultPermissionSupport"]="N" ; //Default permission for built in role Support
-$actionRows[0]["categoryPermissionStaff"]="N" ; //Should this action be available to user roles in the Staff category?
-$actionRows[0]["categoryPermissionStudent"]="N" ; //Should this action be available to user roles in the Student category?
-$actionRows[0]["categoryPermissionParent"]="N" ; //Should this action be available to user roles in the Parent category?
-$actionRows[0]["categoryPermissionOther"]="N" ; //Should this action be available to user roles in the Other category?
+$actionRows[0]['name']='' ; //The name of the action (appears to user in the right hand side module menu)
+$actionRows[0]['precedence']='0'; //If it is a grouped action, the precedence controls which is highest action in group
+$actionRows[0]['category']='' ; //Optional: subgroups for the right hand side module menu
+$actionRows[0]['description']='' ; //Text description
+$actionRows[0]['URLList']='logs_access.php' ; //List of pages included in this action
+$actionRows[0]['entryURL']='logs_access.php'; //The landing action for the page.
+$actionRows[0]['entrySidebar']='Y' ;
+$actionRows[0]['menuShow']='Y' ;
+$actionRows[0]['defaultPermissionAdmin']='Y' ; //Default permission for built in role Admin
+$actionRows[0]['defaultPermissionTeacher']='' ; //Default permission for built in role Teacher
+$actionRows[0]['defaultPermissionStudent']='N' ; //Default permission for built in role Student
+$actionRows[0]['defaultPermissionParent']='N' ; //Default permission for built in role Parent
+$actionRows[0]['defaultPermissionSupport']='N' ; //Default permission for built in role Support
+$actionRows[0]['categoryPermissionStaff']='N' ; //Should this action be available to user roles in the Staff category?
+$actionRows[0]['categoryPermissionStudent']='N' ; //Should this action be available to user roles in the Student category?
+$actionRows[0]['categoryPermissionParent']='N' ; //Should this action be available to user roles in the Parent category?
+$actionRows[0]['categoryPermissionOther']='N' ; //Should this action be available to user roles in the Other category?
 
 //Hooks
-$hooks[0]="" ; //Serialised array to create hook and set options. See Hooks documentation online.
+$hooks[0]='' ; //Serialised array to create hook and set options. See Hooks documentation online.
 ?>
